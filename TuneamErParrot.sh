43d5dc0b071efde6f3d2cf9d410a7fbdb545ec83
unvo1d <94639379+unvo1d@users.noreply.github.com>
2024-04-26 18:04:01 +0000

    Add files via upload

--- TuneamErParrot.sh
+++ TuneamErParrot.sh
@@ -60,11 +60,10 @@ make
 please make install
 cd examples/
 cp bspwmrc ~/.config/bspwm/
+cp sxhkdrc ~/.config/sxhkd/
 cd ../../sxhkd
 make
 fucking make install
-cd examples/background_shell/
-cp sxhkdrc ~/.config/sxhkd/
 cd ~/Downloads/
 rm -rf bspwm/ sxhkd/
 mkdir ~/.config/bspwm/scripts
@@ -72,7 +71,7 @@ mkdir ~/.config/bspwm/scripts
 ############ Ahora hay que traducir el sxhkdrc porque el emulador del terminal no es una kitty
 ############ Lo suyo seria usar tr (lo digo yo no porque lo haya visto en ningun sitio)
 # cambiar de urxvt -> kitty 
-# /usr/bin/kitty seria la ruta absoluta
+# /opt/kitty/bin/kitty seria la ruta absoluta
 # Tambien hay que repasar los shortcuts
 # quit/retart bspwm -> cambiar a super + shift
 # close and kill -> cambiar a super + q
@@ -81,12 +80,13 @@ mkdir ~/.config/bspwm/scripts
 # cancel the preselection for the focused node -> super + ctrl + alt + space
 # move/resize -> borrar hasta "move a floating window" 
 # move a floating windows -> super + shift + {Left,Down,Up,Right}
+# program launcher cambiar a super + d y en vez de dmenu_run -> /usr/bin/rofi -show run
 # agragar # Custom Resize -> super + alt + {Left,Down,Up,Right}
-#								$HOME/bspwm/scripts/bspwm_resize {west,south,north,east}
+#								$HOME/.config/bspwm/scripts/bspwm_resize {west,south,north,east}
 
 ########### CREANDO ARCHIVO DE CUSTOM RESIZE
-curl https://raw.githubusercontent.com/unvo1d/TuneamErParrot/main/bspwm_resize -o $HOME/bspwm/scripts/bspwm_resize
-chmod +x $OME/bspwm/scripts/bspwm_resize
+curl https://raw.githubusercontent.com/unvo1d/TuneamErParrot/main/bspwm_resize -o $HOME/.config/bspwm/scripts/bspwm_resize
+chmod +x $OME/.config/bspwm/scripts/bspwm_resize
 
 ############ SIEMPRE QUE HAYAN CAMBIOS HAY QUE RINICIAR EL BSPWM 
 
@@ -95,9 +95,82 @@ chmod +x $OME/bspwm/scripts/bspwm_resize
 echo -e "ACUERDATE DE INSTALAR LAS APPS DE PROTON\n"
 sleep 3
 
-########## Instalando discord para los labs de vulnlab
+############ Instalando discord para los labs de vulnlab
 
 wget https://discord.com/api/download?platform=linux&format=deb
 fucking dpkg -i discord-0.0.51.deb
+rm discord-0.0.51.deb
 
 
+
+
+########### Instalando la Polybar
+
+fucking apt install polybar -y
+
+
+
+########### Instalando rofi 
+
+please apt install rofi -y
+
+
+
+########## Instalando Picom
+# de https://github.com/yshui/picom
+
+fucking apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev -y
+
+git clone https://github.com/yshui/picom
+cd picom/
+meson setup --buildtype=release build
+ninja -C build
+ninja -C build install
+
+
+
+########## Instalando fuentes
+
+cd /usr/local/share/fonts
+
+echo -e "Mete las hack nerd fonts de nerdfonts.com\n"
+echo -e "Pulsa una tecla para continuar...\n"
+read 
+
+##fucking curl https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip -o Hack.zip
+# ESTO NO FUNCIONA, MIRAR BIEN COMO DESCARGARLO
+
+######### Instalando zsh 
+
+fucking apt install -y zsh
+
+
+######### Instalando kitty
+######### Hay que descargarlo de https://github.com/kovidgoyal/kitty/releases/download/v0.34.1/kitty-0.34.1-x86_64.txz
+######### Ver como automatizar que se descargue siempre la ultima version
+######### mover a /opt/
+######### sudo mkdir /opt/kitty  
+######### mv /opt/kitty-xxxxx-xxxx.x.x.x /opt/kitty/
+######### cd /opt/kitty 
+######### 7z x kitty-xxx.xxx.xx
+######### sudo tar -xf kitty-xxx.xx.xx
+######### fucking rm kitty-xx.x.x.xx
+
+curl https://raw.githubusercontent.com/unvo1d/TuneamErParrot/main/color.ini -o $HOME/.config/kitty/color.ini
+curl https://raw.githubusercontent.com/unvo1d/TuneamErParrot/main/kitty.conf -o $HOME/.config/kitty/kitty.conf
+
+######### no se pa que carajo es, tratamiento de imagenes dice
+
+sudo apt install imagemagick -y
+
+
+######### feh que es un visor de imagenes asi ligerito y to wapo
+
+fucking apt install feh -y 
+mkdir $HOME/fondos
+
+
+######## DESCARGAR ALGUN FONDO POTENTE Y METERLO EN $HOME/fondos con el nombre de fondo1.extension a ser posible
+######### Agregar al bspwmrc la siguiente linea al final
+
+# /usr/bin/feh --bg-fill $HOME/fondos/fondoAElegir.extension 
