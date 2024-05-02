28277b48e55f3d97738685b8e7ce4f5b5a2c8d0c
unvo1d <94639379+unvo1d@users.noreply.github.com>
2024-05-02 10:39:58 +0000

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -12,14 +12,16 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 ## Left bar
 polybar log -c ~/.config/polybar/current.ini &
-polybar secondary -c ~/.config/polybar/current.ini &
+# polybar secondary -c ~/.config/polybar/current.ini &
 polybar ethernet_bar -c ~/.config/polybar/current.ini &
 polybar vpn_bar -c ~/.config/polybar/current.ini &
 
 
 ## Right bar
-polybar top -c ~/.config/polybar/current.ini &
+polybar secondary -c ~/.config/polybar/current.ini &
+polybar target_to_hack -c ~/.config/polybar/current.ini &
 polybar primary -c ~/.config/polybar/current.ini &
+polybar target_to_hack -c ~/.config/polybar/current.ini &
 
 ## Center bar
 polybar primary -c ~/.config/polybar/workspace.ini &
